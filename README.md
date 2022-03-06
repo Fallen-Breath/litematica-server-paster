39b64fb992b870fd5480cc9a3abb1a0ed14f1462
Fallen_Breath <yqx791125833@gmail.com>
2022-03-06 17:33:32 +0800

    entity pasting support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Litematica Server Paster
 
-Let Litematica be able to paste tile entity data in a server
+Let Litematica be able to paste tile entity data of block / entity data in a server
 
 By using a custom chat packet to bypass the chat length limit so the client and simply append the tile entity nbt tag to the `/setblock` command
 
--- src/main/java/me/fallenbreath/lmspaster/mixins/TaskPasteSchematicSetblockMixin.java
+++ src/main/java/me/fallenbreath/lmspaster/mixins/TaskPasteSchematicSetblockMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.arguments.BlockArgumentParser;
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
@@ -46,7 +47,7 @@ public abstract class TaskPasteSchematicSetblockMixin
 			),
 			remap = false
 	)
-	private void useCustomLongChatPacketToPasteNbtDirectly(ClientPlayerEntity player, String string, /* function args -> */ int x, int y, int z, BlockState state, ClientPlayerEntity player_)
+	private void useCustomLongChatPacketToPasteBlockNbtDirectly(ClientPlayerEntity player, String string, /* function args -> */ int x, int y, int z, BlockState state, ClientPlayerEntity player_)
 	{
 		if (ClientNetworkHandler.doesServerAcceptsLongChat())
 		{
@@ -65,6 +66,61 @@ public abstract class TaskPasteSchematicSetblockMixin
 				if (ClientNetworkHandler.canSendCommand(command))
 				{
 					LitematicaServerPasterMod.LOGGER.info("Pasting block {} at [{}, {}, {}] with nbt tag", state.getBlock().getName().getString(), x, y, z);
+					ClientNetworkHandler.sendCommand(command);
+					return;
+				}
+			}
+		}
+		// original behavior
+		player.sendChatMessage(string);
+	}
+
+	private Entity currentEntity;
+
+	@ModifyVariable(
+			method = "summonEntities",
+			at = @At(
+					value = "STORE",
+					target = "Ljava/util/Iterator;next()Ljava/lang/Object;",
+					remap = false
+			),
+			remap = false,
+			ordinal = 0
+	)
+	private Entity recordCurrentEntity(Entity entity)
+	{
+		this.currentEntity = entity;
+		return entity;
+	}
+
+	@Redirect(
+			method = "summonEntities",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/network/ClientPlayerEntity;sendChatMessage(Ljava/lang/String;)V",
+					remap = true
+			),
+			remap = false
+	)
+	private void useCustomLongChatPacketToPasteEntityNbtDirectly(ClientPlayerEntity player, String string)
+	{
+		if (ClientNetworkHandler.doesServerAcceptsLongChat())
+		{
+			if (this.currentEntity != null)
+			{
+				CompoundTag tag = this.currentEntity.toTag(new CompoundTag());
+
+				// like net.minecraft.client.Keyboard.copyEntity
+				tag.remove("UUIDMost");
+				tag.remove("UUIDLeast");
+				tag.remove("Pos");
+				tag.remove("Dimension");
+
+				String tagString = tag.toString();
+				String command = string + " " + tagString;
+				if (ClientNetworkHandler.canSendCommand(command))
+				{
+					LitematicaServerPasterMod.LOGGER.info("Summoning entity {} with nbt tag", this.currentEntity.getType().getName().getString());
 					ClientNetworkHandler.sendCommand(command);
 					return;
 				}
