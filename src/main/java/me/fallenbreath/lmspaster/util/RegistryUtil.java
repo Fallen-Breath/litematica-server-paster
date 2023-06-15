7af24e8453e2a07460ee508132beede205d4407a
Fallen_Breath <yqx791125833@gmail.com>
2023-06-16 00:59:42 +0800

    LGPLv3

--- src/main/java/me/fallenbreath/lmspaster/util/RegistryUtil.java
+++ src/main/java/me/fallenbreath/lmspaster/util/RegistryUtil.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the Litematica Server Paster project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2023  Fallen_Breath and contributors
+ *
+ * Litematica Server Paster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Litematica Server Paster is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Litematica Server Paster.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.fallenbreath.lmspaster.util;
 
 import me.fallenbreath.lmspaster.LitematicaServerPasterMod;
